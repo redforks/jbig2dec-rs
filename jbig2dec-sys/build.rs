f3dc17af12303822c05cae568f2c1f30b7fb8db6
messense <messense@icloud.com>
2019-12-16 16:14:17 +0800

    Update Cargo metadata and add license

--- jbig2dec-sys/build.rs
+++ jbig2dec-sys/build.rs
@@ -1,24 +1,45 @@
+use std::fs;
+
+fn fail_on_empty_directory(name: &str) {
+    if fs::read_dir(name).unwrap().count() == 0 {
+        println!(
+            "The `{}` directory is empty, did you forget to pull the submodules?",
+            name
+        );
+        println!("Try `git submodule update --init --recursive`");
+        panic!();
+    }
+}
+
 fn main() {
+    fail_on_empty_directory("jbig2dec");
+
+    println!("cargo:rerun-if-changed=config.h");
+    println!("cargo:rerun-if-changed=config_types.h");
+    let src_files = vec![
+        "jbig2dec/jbig2_arith.c",
+        "jbig2dec/jbig2_arith_int.c",
+        "jbig2dec/jbig2_arith_iaid.c",
+        "jbig2dec/jbig2_huffman.c",
+        "jbig2dec/jbig2_hufftab.c",
+        "jbig2dec/jbig2_segment.c",
+        "jbig2dec/jbig2_page.c",
+        "jbig2dec/jbig2_symbol_dict.c",
+        "jbig2dec/jbig2_text.c",
+        "jbig2dec/jbig2_halftone.c",
+        "jbig2dec/jbig2_generic.c",
+        "jbig2dec/jbig2_refinement.c",
+        "jbig2dec/jbig2_mmr.c",
+        "jbig2dec/jbig2_image.c",
+        "jbig2dec/jbig2.c",
+    ];
+    for file in &src_files {
+        println!("cargo:rerun-if-changed={}", file);
+    }
     cc::Build::new()
         .include(".")
         .include("jbig2dec/")
-        .files(vec![
-            "jbig2dec/jbig2_arith.c",
-            "jbig2dec/jbig2_arith_int.c",
-            "jbig2dec/jbig2_arith_iaid.c",
-            "jbig2dec/jbig2_huffman.c",
-            "jbig2dec/jbig2_hufftab.c",
-            "jbig2dec/jbig2_segment.c",
-            "jbig2dec/jbig2_page.c",
-            "jbig2dec/jbig2_symbol_dict.c",
-            "jbig2dec/jbig2_text.c",
-            "jbig2dec/jbig2_halftone.c",
-            "jbig2dec/jbig2_generic.c",
-            "jbig2dec/jbig2_refinement.c",
-            "jbig2dec/jbig2_mmr.c",
-            "jbig2dec/jbig2_image.c",
-            "jbig2dec/jbig2.c",
-        ])
+        .files(src_files)
         .warnings(false)
         .extra_warnings(false)
         .compile("libjbig2dec.a");
