f2d536c56bd4aba8b2803d1e9c133c36e4bd45fa
messense <messense@icloud.com>
2019-12-16 16:03:09 +0800

    Impl std::error::Error for Error

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,5 @@
+use std::error;
+use std::fmt;
 use std::io;
 
 #[derive(Debug)]
@@ -11,5 +13,24 @@ pub enum Error {
 impl From<io::Error> for Error {
     fn from(err: io::Error) -> Self {
         Self::Io(err)
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            Error::Io(ref e) => e.fmt(f),
+            Error::CreateContextFailed => write!(f, "create context failed"),
+            Error::IncompletePage => write!(f, "incomplete page"),
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match *self {
+            Error::Io(ref e) => Some(e),
+            _ => None,
+        }
     }
 }
